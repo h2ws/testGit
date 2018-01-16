990fa1de9720c1741363d90eb8c5f688e8c34899
oyydd <34313916+oyydd@users.noreply.github.com>
2018-01-16 23:39:42 +0800

    创建C#.cs
    第一次使用github创建项目文件

---
+++ python.py
@@ -0,0 +1,16 @@
+def quickSort(arr,begin,end):
+    if(begin >= end):
+        return
+    else:
+        x = arr[begin]
+        p1,p2 = begin,end
+        dr = True
+        while(i<j):
+          if(dr):
+            i = p2
+            while(p1<i):
+            else:
+
+        else:
+
+  
